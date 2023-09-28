e6f7dfdac87a4d333095448f8f48734e2d301694
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-28 11:35:55 +0900

    Update activity.services.js

--- src/services/activity.services.js
+++ src/services/activity.services.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+//get all activity
 export const getActivities = (callback) => {
     axios
-    .get('https://todo.api.devcode.gethired.id/activity-groups')
+    // .get('https://todo.api.devcode.gethired.id/activity-groups')
+    .get('https://todo.api.devcode.gethired.id/activity-groups?email=bernadettechrestella%40gmail.com')
     .then((res) => {
         callback(res.data.data)
     })
@@ -11,6 +13,7 @@ export const getActivities = (callback) => {
     })
 }
 
+//get detail acivity
 export const getActivityById = (id, callback) => {
     axios
         .get(`https://todo.api.devcode.gethired.id/activity-groups/${id}`)
@@ -22,17 +25,27 @@ export const getActivityById = (id, callback) => {
         })
 }
 
-export const createActivity = (newActivity, callback) => {
+//create new activity
+export const createActivity = (title = "New Activity", email= "bernadettechrestella@gmail.com", callback) => {
+    const data = {
+        title: title,
+        email: email,
+        _comment: "Aktivitas baru"
+    }
+    console.log("createActivity"+ data)
+
     axios
-    .post('https://todo.api.devcode.gethired.id/activity-groups', newActivity)
-    .then((res) => {
-        callback(res.data.data)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        .post('https://todo.api.devcode.gethired.id/activity-groups', data)
+        .then((res) => {
+            callback(res.data)
+            console.log("createActivity"+ res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
+//delete activity
 export const deleteActivity = (id, callback) => {
     axios
     .delete(`https://todo.api.devcode.gethired.id/activity-groups/${id}`)
@@ -41,5 +54,16 @@ export const deleteActivity = (id, callback) => {
     })
     .catch((err) => {
         console.log(err)
+    })
+}
+
+export const updateActivity = (id, newData, callback) => {
+  axios
+    .put(`https://todo.api.devcode.gethired.id/activity-groups/${id}`, newData)
+    .then((res) => {
+      callback(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
     })
 }
