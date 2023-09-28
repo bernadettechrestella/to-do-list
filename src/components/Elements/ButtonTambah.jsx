70f8f86fb51dc555ae3b522e539579e99cdbc025
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-28 11:35:44 +0900

    Update ButtonTambah.jsx

--- src/components/Elements/ButtonTambah.jsx
+++ src/components/Elements/ButtonTambah.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 
-const ButtonTambah = () => {
+const ButtonTambah = (props) => {
+  const {
+    onClick = () => {}
+  } = props;
+
   return (
-    <button className='text-white font-poppins font-semibold text-lg bg-blueSky w-[159px] h-[54px] rounded-full'>
+    <button className='text-white font-poppins font-semibold text-lg bg-blueSky w-[159px] h-[54px] rounded-full'
+      onClick={onClick}>
         <div className='flex items-center justify-center gap-[6px]'>
             <AiOutlinePlus size={24}/>
             <p>Tambah</p>
