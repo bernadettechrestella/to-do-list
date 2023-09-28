c82108f82d984e8d50cb608833d264980590aaac
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-28 11:35:41 +0900

    Update CardList.jsx

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -19,13 +19,13 @@ const CardList = (props) => {
   return (
     <div className='flex justify-between bg-white h-[80px] px-[28px] rounded-lg shadow-lg mb-[10px]'>
         <div className="flex text-center items-center">
-            <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blueSky bg-white border-gray rounded focus:ring-blueSky focus:ring-2 mr-[22px]"/>
-            <BsDot size={50} className={dotColorClass}/>
-            <label htmlFor="default-checkbox" className="text-lg font-medium mx-[16px]">{title}</label>
-            <LuPencil className='text-gray' size={15}/>
+            <input data-cy="todo-item-checkbox" id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blueSky bg-white border-gray rounded focus:ring-blueSky focus:ring-2 mr-[22px]"/>
+            <BsDot data-cy="todo-item-priority-indicator" size={50} className={dotColorClass}/>
+            <label data-cy="todo-item-title" htmlFor="default-checkbox" className="text-lg font-medium mx-[16px]">{title}</label>
+            <LuPencil data-cy="todo-item-edit-button" className='text-gray' size={15}/>
         </div>
         <div className='flex text-center items-center'>
-            <HiOutlineTrash size={24} className='text-gray'/>
+            <HiOutlineTrash data-cy="todo-item-delete-button" size={24} className='text-gray'/>
         </div>
     </div>
   )
